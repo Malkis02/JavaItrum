66610e633ab11f211f6a05565705a044a004b592
Malkis <gancevk@yandex.ru>
2024-03-15 14:46:26 +0500

    refactor for stream

--- CollectionCount/src/Main.java
+++ CollectionCount/src/Main.java
@@ -1,25 +1,30 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         String[] arr = new String[]{"a","b","c","a","b","c","c","b","d"};
-        System.out.println(countOfElements(arr));
+        Map<String,Long> countMap = Arrays.stream(arr)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        System.out.println(countMap);
     }
 
-    public static Map countOfElements(String[] arr){
-        Map<String,Integer> elements = new HashMap<>();
-        int count = 0;
-        for(int j = 0; j < arr.length; j++) {
-            for (int i = 0; i < arr.length; i++) {
-                String str = arr[j];
-                if (arr[j].equals(arr[i])) {
-                    count++;
-                    elements.put(str, count);
-                }
-            }
-            count = 0;
-        }
-        return elements;
-    }
+//    public static Map countOfElements(String[] arr){
+//        Map<String,Integer> elements = new HashMap<>();
+//        int count = 0;
+//        for(int j = 0; j < arr.length; j++) {
+//            for (int i = 0; i < arr.length; i++) {
+//                String str = arr[j];
+//                if (arr[j].equals(arr[i])) {
+//                    count++;
+//                    elements.put(str, count);
+//                }
+//            }
+//            count = 0;
+//        }
+//        return elements;
+//    }
 }
