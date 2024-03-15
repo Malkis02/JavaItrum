b86f2da84e0dfdcb1cf69d4305856af31dff54c8
Malkis <gancevk@yandex.ru>
2024-03-15 12:34:34 +0500

    StreamPrac

---
+++ StreamGenerator/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0),
+                new Order("PC",2000.0)
+        );
+                orders.stream()
+                        .collect(Collectors.groupingBy(Order::getProduct, Collectors.summingDouble(Order::getCost)))
+                        .entrySet()
+                        .stream()
+                        .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                        .limit(3)
+                        .forEach(System.out::println);
+    }
+}
